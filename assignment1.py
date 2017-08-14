7697e8b6e4fd9cb5ebdb147acfd2b0501719334d
Lindsay Ward <lindsay.ward@jcu.edu.au>
2017-08-15 09:38:06 +1000

    Initial commit

---
+++ assignment1.py
@@ -0,0 +1,11 @@
+"""
+Replace the contents of this module docstring with your own details.
+"""
+
+
+def main():
+    print("Songs To Learn 1.0 - by <Your Name>")
+
+
+if __name__ == '__main__':
+    main()
